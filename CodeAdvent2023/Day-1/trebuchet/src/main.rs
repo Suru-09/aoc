009fe3f5eb917016c6e1e676abfae91d12d7e2c3
Surugiu Dragos <dragos.surugiu13@yahoo.com>
2023-12-08 09:05:47 +0200

    Added Part 2 which for some reason does not work

--- CodeAdvent2023/Day-1/trebuchet/src/main.rs
+++ CodeAdvent2023/Day-1/trebuchet/src/main.rs
@@ -47,69 +47,91 @@ mod part_2 {
     use std::fs::File;
     use std::io::{BufRead, BufReader};
 
-    const DIGITS_AS_LETTER_VEC: [&str; 10] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    const DIGITS: [i64; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9];
+   const DIGIT_TO_STR_MAP: [(usize, &str); 9] = [
+        (1, "one"),
+        (2, "two"),
+        (3, "three"),
+        (4, "four"),
+        (5, "five"),
+        (6, "six"),
+        (7, "seven"),
+        (8, "eight"),
+        (9, "nine")
+   ];
 
-    fn extract_calibration_number(line: &str) -> i64 {
-        let mut calibration_number: i64 = 0;
-        let mut idx: i64 = 0;
+   fn get_digit_after_digit_as_string(str: String) -> usize {
+        DIGIT_TO_STR_MAP.iter().find(|digit_tuple| {
+            str.contains(digit_tuple.1)
+        }).unwrap().0
+   }
 
-        let idx_of_digit_as_string = |digit_as_string: &str| -> i64 {
-            for (idx, digit) in DIGITS_AS_LETTER_VEC.iter().enumerate() {
-                if digit_as_string.find(digit) != None {
-                    return idx as i64;
-                }
-            }
-            -1
-        };
+   fn extract_calibration_number(line: &str) -> i64 {
+        let mut calibration_number: i64 = i64::MIN;
+        let mut line_as_string = line.to_string();
 
-        for character in line.chars() {
-            idx += 1;
-            if character.is_digit(10) {
-                break;
+        let mut digits_string: Vec<(usize, String)> = Vec::new();
+        for (digit, digit_as_string) in DIGIT_TO_STR_MAP.iter() {
+            if line_as_string.contains(digit_as_string) {
+                let digit_index = line_as_string.find(digit_as_string).unwrap();
+                digits_string.push((digit_index, digit_as_string.to_string()));
             }
         }
 
-        let idx_from_strings: i64 = idx_of_digit_as_string(line);
+        digits_string.sort_by(|a, b| a.0.cmp(&b.0));
         
-        if idx_from_strings != -1 && idx_from_strings >= idx {
-            calibration_number = DIGITS.get(idx_from_strings as usize).unwrap().clone();
-        }
-        else if idx_from_strings != -1 && idx_from_strings < idx {
-            calibration_number = line.chars().nth(idx_from_strings as usize).unwrap().to_digit(10).unwrap() as i64;
-        }
+        // replace only first and last occurence of digit string
+        if  digits_string.len() > 0 {
+            let first_digit_as_string = digits_string.first().unwrap().1.to_string();
+            let digit_after_digit_as_string = get_digit_after_digit_as_string(first_digit_as_string.clone());
 
-        idx = 0;
-        for character in line.chars().rev() {
-            idx += 1;
-            if character.is_digit(10) {
-                break;
+            line_as_string = line_as_string.replacen(&first_digit_as_string, &digit_after_digit_as_string.to_string(), 1);
+            if digits_string.len() > 1 {
+                let last_digit_as_string = digits_string.last().unwrap().1.to_string();
+                let digit_after_digit_as_string = get_digit_after_digit_as_string(last_digit_as_string.clone());
+
+                line_as_string = line_as_string.replacen(&last_digit_as_string, &digit_after_digit_as_string.to_string(), 1);
             }
         }
 
-        let idx_of_digit_as_string_in_reverse = |digit_as_string: &str| -> i64 {
-            for (idx, digit) in DIGITS_AS_LETTER_VEC.iter().rev().enumerate() {
-                if digit_as_string.find(digit) != None {
-                    return idx as i64;
+        println!("{} -> {}", line, line_as_string);
+
+        for character in line_as_string.chars() {
+            if character.is_digit(10) && calibration_number==i64::MIN {
+                calibration_number = character.to_digit(10).unwrap() as i64;
+                for character in line_as_string.chars().rev() {
+                    if character.is_digit(10) {
+                        calibration_number = calibration_number * 10 + character.to_digit(10).unwrap() as i64;
+                        break;
+                    }
                 }
             }
-            -1
-        };
-
-        let idx_from_strings_in_reverse: i64 = idx_of_digit_as_string_in_reverse(line);
-
-        if idx_from_strings_in_reverse != -1 && idx_from_strings_in_reverse >= idx {
-            calibration_number = calibration_number * 10 + DIGITS[idx_from_strings_in_reverse];
         }
-        else if idx_from_strings_in_reverse != -1 && idx_from_strings_in_reverse < idx {
-            calibration_number = calibration_number * 10 + line.chars().rev().nth(idx_from_strings_in_reverse).unwrap().to_digit(10).unwrap() as i64;
+
+        if calibration_number == i64::MIN {
+            calibration_number = 0;
         }
+        
 
         calibration_number
-    }
+   }    
+
+   pub fn solve_part_2() {
+        let file = File::open("part2.txt").unwrap();
+        let reader = BufReader::new(file);
+
+        let mut total: i64 = 0;
+        for line in reader.lines() {
+            let line = line.unwrap();
+            let calibration_number = extract_calibration_number(&line);
+            total += calibration_number;
+        }
+
+        println!("Part 2: {}", total);
+   }
 }
 
 
 fn main() {
     part_1::solve_part_1();
+    part_2::solve_part_2();
 }
