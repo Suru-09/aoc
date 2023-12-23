c051d46cff84dfa77c6a27e7afda751d6832c0a7
Surugiu Dragos <dragos.surugiu13@yahoo.com>
2023-12-23 11:40:16 +0200

    Added problem 5

--- 2023/Day-5/seed-fertilizer/src/main.rs
+++ 2023/Day-5/seed-fertilizer/src/main.rs
@@ -1,17 +1,64 @@
-use std::collections::HashMap;
-
 mod part_1 {
     use crate::parse_seeds_and_maps;
 
     pub fn solve() {
+        let mut results: Vec<u64> = vec![];
         let seed_fertilizers = parse_seeds_and_maps();
-        seed_fertilizers.get_location_for_seed(79);
-        println!("{:?}", seed_fertilizers);
+
+        for seed_id in 0..seed_fertilizers.seeds.len() {
+            let mut value = seed_fertilizers.seeds[seed_id];
+            for map_id in 0..seed_fertilizers.maps.len() {
+                value = match seed_fertilizers.get_location_for_seed(value, map_id) {
+                    Some(location) => location,
+                    None => value
+                };
+                if map_id == 6 {
+                    results.push(value);
+                    break;
+                }
+            }
+        }
+        let result = results.iter().min().unwrap_or(&0);
+        println!("Result is : {}", result);
     }
 }
 
 mod part_2 {
+    use crate::parse_seeds_and_maps;
 
+    pub fn solve() {
+        let mut results: Vec<u64> = vec![];
+        let seed_fertilizers = parse_seeds_and_maps();
+
+        let seeds = get_seeds_given_ranges(seed_fertilizers.seeds.clone());
+        for seed_id in 0..seeds.len() {
+            let mut value = seeds[seed_id];
+            for map_id in 0..seed_fertilizers.maps.len() {
+                value = match seed_fertilizers.get_location_for_seed(value, map_id) {
+                    Some(location) => location,
+                    None => value
+                };
+                if map_id == 6 {
+                    results.push(value);
+                    break;
+                }
+            }
+        }
+        let result = results.iter().min().unwrap_or(&0);
+        println!("Result is : {}", result);
+    }
+
+    fn get_seeds_given_ranges(seeds_ranges: Vec<u64>) -> Vec<u64> {
+        let mut seeds: Vec<u64> = vec![];
+        for idx in (0..seeds_ranges.len()).step_by(2) {
+            let range_start = seeds_ranges[idx];
+            let range_length = seeds_ranges[idx + 1];
+            for offset in 0..range_length {
+                seeds.push(range_start + offset);
+            }
+        }
+        seeds
+    }
 }
 
 
@@ -36,7 +83,6 @@ fn parse_seeds_and_maps() -> SeedFertilizer {
             ).collect::<Vec<u64>>();
     input.remove(0);
 
-    let mut current_map_name: String = String::new();
     let mut current_range_vec = Vec::new();
     input
         .iter()
@@ -55,6 +101,10 @@ fn parse_seeds_and_maps() -> SeedFertilizer {
                 current_range_vec.push(Range::new(range_vec[0], range_vec[1], range_vec[2]));
             }
         });
+        if !current_range_vec.is_empty() {
+            maps.push(current_range_vec.clone());
+            current_range_vec.clear();
+        }
     SeedFertilizer::new(seeds, maps)
 }
 
@@ -74,8 +124,11 @@ impl Range {
 
     pub fn mapping_for_number(&self, num: u64) -> Option<u64> {
         if self.source + self.length >= num {
-            let needed_length = self.length + self.source - num;
-            return Some(self.destination + needed_length);
+            for i in 0..self.length {
+                if self.source + i == num {
+                    return Some(i + self.destination)
+                }
+            }
         }
         None
     }
@@ -96,18 +149,21 @@ impl SeedFertilizer {
         }
     }
 
-    fn get_location_for_seed(&self, seed: u64) -> Option<u64> {
-        let first_vec = self.maps.first().unwrap();
-        let value = first_vec.iter()
-            .map(|range| {
-                range.mapping_for_number(seed)
-            }).collect::<Vec<Option<u64>>>();
-        println!("{:?}", value.iter().min());
-        Some(1)
+    fn get_location_for_seed(&self, seed: u64, map_number: usize) -> Option<u64> {
+        let map = self.maps.get(map_number).unwrap();
+        for range in map {
+            if range.mapping_for_number(seed).is_some() {
+                return range.mapping_for_number(seed);
+            }
+        }
+        None
     }
+
+
 }
 
 
 fn main() {
-    part_1::solve();
+    part_1::solve(); // 174137457
+    part_2::solve(); // 1493866
 }
