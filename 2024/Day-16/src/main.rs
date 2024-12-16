208b1a461d0df4611b593cae78dcd6638ce6bcfc
Suru-09 <dragos.surugiu13@yahoo.com>
2024-12-16 21:56:19 +0200

    Added Day-16 part 1

---
+++ 2024/Day-16/src/main.rs
@@ -0,0 +1,241 @@
+use std::collections::HashMap;
+use std::collections::VecDeque;
+
+const INPUT: &str = "input.txt";
+const EXAMPLE: &str = "example.txt";
+
+const DIRS: [(isize, isize); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+
+pub fn solve_part_1() {
+    let maze_str = utils::read_file(EXAMPLE);
+    let maze = parse_maze(&maze_str);
+    let (start, end) = (find_symbol(&maze, 'S'), find_symbol(&maze, 'E'));
+    let visited = lowest_score(&maze, start);
+    let mut result = usize::MAX;
+    for (key, val) in visited.iter() {
+        if key.0 == end.0 && key.1 == end.1 {
+            result = std::cmp::min(result, *val);
+        }
+    }
+
+    println!("Result for part1: {}", result);
+}
+
+pub fn solve_part_2() {
+    let maze_str = utils::read_file(EXAMPLE);
+    let maze = parse_maze(&maze_str);
+    let (start, end) = (find_symbol(&maze, 'S'), find_symbol(&maze, 'E'));
+    let visited = lowest_score(&maze, start);
+    let mut best_cost = usize::MAX;
+    for (key, val) in visited.iter() {
+        if key.0 == end.0 && key.1 == end.1 {
+            best_cost = std::cmp::min(best_cost, *val);
+        }
+    }
+
+    let result = recreate_path(&maze, end, &visited, best_cost);
+    println!("Result for part2: {}", result);
+}
+
+fn find_symbol(maze: &Vec<Vec<char>>, symbol: char) -> (usize, usize) {
+    for i in 0..maze.len() {
+        for j in 0..maze[i].len() {
+            if maze[i][j] == symbol {
+                return (i, j);
+            }
+        }
+    }
+    (0, 0)
+}
+
+fn is_out_of_bounds(
+    sizes: (usize, usize),
+    old: (usize, usize),
+    dir: usize,
+    backwards: bool,
+) -> bool {
+    let (width, height) = (sizes.0 as isize, sizes.1 as isize);
+    let (old_x, old_y) = (old.0 as isize, old.1 as isize);
+    let (off_x, off_y) = DIRS[dir];
+
+    if backwards {
+        if old_x - off_x < 0 || old_x - off_x >= width || old_y < 0 || old_y - off_y >= height {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if old_x + off_x < 0 || old_x + off_x >= width || old_y < 0 || old_y + off_y >= height {
+        true
+    } else {
+        false
+    }
+}
+
+fn lowest_score(
+    maze: &Vec<Vec<char>>,
+    start: (usize, usize),
+) -> HashMap<(usize, usize, usize), usize> {
+    let mut queue = VecDeque::new();
+    let start_dir = (start.0, start.1, 1);
+    queue.push_back((start_dir, 0));
+    let (width, height) = (maze[0].len(), maze.len());
+    let mut visited = HashMap::new();
+    visited.insert(start_dir, usize::MAX);
+
+    while let Some(((x, y, dir), cost)) = queue.pop_front() {
+        if cost > *visited.get(&(x, y, dir)).unwrap() {
+            continue;
+        }
+
+        if !is_out_of_bounds((width, height), (x, y), dir, false) {
+            let (new_x, new_y) = (
+                (x as isize + DIRS[dir].0) as usize,
+                (y as isize + DIRS[dir].1) as usize,
+            );
+            let new_cost = cost + 1;
+            let new_pos = ((new_x, new_y, dir), new_cost);
+
+            if maze[new_x][new_y] != '#' {
+                match visited.get_mut(&new_pos.0) {
+                    Some(visited_val) => {
+                        if maze[new_x][new_y] != '#' && *visited_val > new_cost {
+                            *visited_val = new_cost;
+                            queue.push_back(new_pos);
+                        }
+                    }
+                    None => {
+                        visited.insert(new_pos.0, new_pos.1);
+                        queue.push_back(new_pos);
+                    }
+                }
+            }
+        }
+
+        // turn left/right
+        for d in get_left_right(dir).into_iter() {
+            if !is_out_of_bounds((width, height), (x, y), d, false) {
+                let (new_x, new_y) = (
+                    (x as isize + DIRS[d].0) as usize,
+                    (y as isize + DIRS[d].1) as usize,
+                );
+                let new_cost = cost + 1001;
+                let new_pos = ((new_x, new_y, d), new_cost);
+
+                if maze[new_x][new_y] != '#' {
+                    match visited.get_mut(&new_pos.0) {
+                        Some(visited_val) => {
+                            if maze[new_x][new_y] != '#' && *visited_val > new_cost {
+                                *visited_val = new_cost;
+                                queue.push_back(new_pos);
+                            }
+                        }
+                        None => {
+                            visited.insert(new_pos.0, new_pos.1);
+                            queue.push_back(new_pos);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    visited
+}
+
+fn recreate_path(
+    maze: &Vec<Vec<char>>,
+    end: (usize, usize),
+    visited: &HashMap<(usize, usize, usize), usize>,
+    result_cost: usize,
+) -> usize {
+    let mut queue = VecDeque::new();
+    for ((x, y, dir), cost) in visited {
+        if *x == end.0 && *y == end.1 && *cost == result_cost {
+            queue.push_back((*x, *y, *dir));
+        }
+    }
+
+    let (width, height) = (maze[0].len(), maze.len());
+    let mut shortest_path = vec![];
+
+    while let Some((x, y, dir)) = queue.pop_front() {
+        let current_cost = *visited.get(&(x, y, dir)).unwrap();
+
+        if !is_out_of_bounds((width, height), (x, y), dir, true) {
+            let (new_x, new_y) = (
+                (x as isize - DIRS[dir].0) as usize,
+                (y as isize - DIRS[dir].1) as usize,
+            );
+            if current_cost > 0 {
+                let backwards_cost = current_cost - 1;
+                if visited.get(&(new_x, new_y, dir)).is_some()
+                    && *visited.get(&(new_x, new_y, dir)).unwrap() == backwards_cost
+                {
+                    shortest_path.push((x, y, dir));
+                    queue.push_back((new_x, new_y, dir));
+                }
+            }
+        }
+
+        // turn left/right
+        for d in get_left_right(dir).into_iter() {
+            if !is_out_of_bounds((width, height), (x, y), d, true) {
+                let (new_x, new_y) = (
+                    (x as isize - DIRS[d].0) as usize,
+                    (y as isize - DIRS[d].1) as usize,
+                );
+
+                if current_cost > 0 {
+                    let backwards_cost = current_cost - 1001;
+                    if visited.get(&(new_x, new_y, d)).is_some()
+                        && *visited.get(&(new_x, new_y, d)).unwrap() == backwards_cost
+                    {
+                        shortest_path.push((x, y, dir));
+                        queue.push_back((new_x, new_y, d));
+                    }
+                }
+            }
+        }
+    }
+
+    println!("Shortest path: {:?}", shortest_path);
+    shortest_path.len()
+}
+
+fn get_left_right(dir: usize) -> Vec<usize> {
+    match dir {
+        0 | 2 => vec![1, 3],
+        1 | 3 => vec![0, 2],
+        _ => panic!("Out of bounds"),
+    }
+}
+
+fn parse_maze(input: &str) -> Vec<Vec<char>> {
+    input
+        .split("\n")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|str| str.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>()
+}
+
+mod utils {
+    pub fn read_file(path: &str) -> String {
+        std::fs::read_to_string(path).unwrap()
+    }
+
+    pub fn read_file_lines(path: &str) -> Vec<String> {
+        std::fs::read_to_string(path)
+            .unwrap()
+            .lines()
+            .map(|line| line.to_string())
+            .collect::<Vec<_>>()
+    }
+}
+
+fn main() {
+    solve_part_1();
+    solve_part_2();
+}
